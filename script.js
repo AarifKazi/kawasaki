b4d094cf5ccd8fc70a21c0bcb009a63173f76cce
AarifKazi <Kaziaarif61@gmail.com>
2024-06-20 07:32:27 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,59 @@
+var x = document.querySelector(".dot-1")
+var y = document.querySelector(".b0x")
+y.style.display = "none";
+x.addEventListener("click", function() {
+    
+    if (y.style.display === "none") {
+        y.style.display = "flex";
+    } else {
+        y.style.display = "none";
+    }
+});
+
+var a = document.querySelector(".dot-2")
+var b = document.querySelector(".b0x-2")
+b.style.display = "none";
+a.addEventListener("click", function() {
+    
+    if (b.style.display === "none") {
+        b.style.display = "flex";
+    } else {
+        b.style.display = "none";
+    }
+});
+
+var c = document.querySelector(".dot-3")
+var d = document.querySelector(".b0x-3")
+d.style.display = "none";
+c.addEventListener("click", function() {
+    
+    if (d.style.display === "none") {
+        d.style.display = "flex";
+    } else {
+        d.style.display = "none";
+    }
+});
+
+var e = document.querySelector(".dot-4")
+var f = document.querySelector(".b0x-4")
+f.style.display = "none";
+e.addEventListener("click", function() {
+    
+    if (f.style.display === "none") {
+        f.style.display = "flex";
+    } else {
+        f.style.display = "none";
+    }
+});
+
+var g = document.querySelector(".dot-5")
+var h = document.querySelector(".b0x-5")
+h.style.display = "none";
+g.addEventListener("click", function() {
+    
+    if (h.style.display === "none") {
+        h.style.display = "flex";
+    } else {
+        h.style.display = "none";
+    }
+});
